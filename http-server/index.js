76b0c68bae603d4b540bffdf7d9a0ae1ebc65cb1
Meghana Vasanthi <130841184+Vasanthi0812@users.noreply.github.com>
2023-12-04 23:45:06 +0530

    commit and push

--- http-server/index.js
+++ http-server/index.js
@@ -1,25 +1,49 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const argv = require('minimist')(process.argv.slice(2));
-const port = argv.port || 5000; // Use the supplied port or default to 3000
+const http = require("http");
+const fs = require("fs");
+const args = require("minimist")(process.argv);
+const port = args.port;
+let homepage = "";
+let projectpage = "";
+let registrationpage = "";
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/registration') {
-    const filePath = path.join(__dirname, 'registration.html');
-    const readStream = fs.createReadStream(filePath);
-    readStream.pipe(res);
-  } else if (req.url === '/') {
-    // Handle other routes or serve home.html
-    const filePath = path.join(__dirname, 'home.html');
-    const readStream = fs.createReadStream(filePath);
-    readStream.pipe(res);
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found');
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
   }
+  homepage = home;
 });
 
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectpage = project;
 });
+
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationpage = registration;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectpage);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registrationpage);
+        response.end();
+        break;
+      default:
+        response.write(homepage);
+        response.end();
+        break;
+    }
+  })
+  .listen(port);
